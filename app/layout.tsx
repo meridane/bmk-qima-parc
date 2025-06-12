9e7efe77b3305d9ab307b6a31e4820a301607641
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 18:19:48 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,21 @@
-'use client';
+import Providers from "./providers";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-
-import '../styles/globals.css';
-import { Inter } from 'next/font/google';
-import SidebarWrapper from '../components/SidebarWrapper';
-import { SessionProvider } from 'next-auth/react';
-
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'BMK Qima Parc',
-  description: 'Application de gestion des chargements de conteneurs',
+  title: "BMK Qima Parc",
+  description: "Plateforme de gestion de conteneurs",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body className={inter.className}>
-        <SessionProvider>
-          <SidebarWrapper>{children}</SidebarWrapper>
-        </SessionProvider>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
