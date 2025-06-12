01993d6d01bd52ad39d55684fe1631dc15e79972
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 11:52:10 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-// app/page.tsx
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-  redirect('/login');
+  return (
+    <html>
+      <head>
+        <meta httpEquiv="refresh" content="0; URL='/login'" />
+      </head>
+      <body>
+        <p>Redirection vers /login...</p>
+      </body>
+    </html>
+  );
 }
