b90c655a0f0968e7e57f718a51df1f0d3c56f07c
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 17:46:17 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import './styles/globals.css';
 import { Inter } from 'next/font/google';
 import SidebarWrapper from './components/SidebarWrapper';
 import { SessionProvider } from 'next-auth/react';
