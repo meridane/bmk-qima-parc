f3dca1bc54bad7722f1047697816bb323b3925e6
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 13:11:16 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
-// app/layout.tsx
-import '../styles/globals.css';
-import { ReactNode } from 'react';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import SidebarWrapper from './components/SidebarWrapper';
+import { SessionProvider } from 'next-auth/react';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'BMK Qima Parc',
-  description: 'Gestion des voitures et conteneurs',
+  description: 'Application de gestion des chargements de conteneurs',
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider>
+          <SidebarWrapper>{children}</SidebarWrapper>
+        </SessionProvider>
+      </body>
     </html>
   );
 }
