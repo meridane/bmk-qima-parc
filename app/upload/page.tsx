ebeae44d49d2eabe7888316ad89ec2dfab2b3744
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 10:48:03 +0900

    Update page.tsx

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import { supabase } from './lib/supabase';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
-import './styles/globals.css';
+import { supabase } from '@/lib/supabase';
+import Sidebar from '@/components/Sidebar';
+import Navbar from '@/components/Navbar';
+import '@/styles/globals.css';
 
 
 export default function UploadVoiture() {
