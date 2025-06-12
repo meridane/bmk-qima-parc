edfd0b7bcfcb488842d7d5a0f5394930fd5daab7
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 17:50:19 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
-import SidebarWrapper from './components/SidebarWrapper';
+import SidebarWrapper from '../components/SidebarWrapper';
 import { SessionProvider } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] });
