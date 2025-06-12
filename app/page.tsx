1741335cfb8ccafa71473c888e3c24891017145a
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 11:30:08 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,6 @@
 // app/page.tsx
-'use client';
+import { redirect } from 'next/navigation';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-export default function HomeRedirect() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/login'); // Redirige automatiquement vers /login
-  }, [router]);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center text-gray-600">
-      Redirection...
-    </div>
-  );
+export default function Home() {
+  redirect('/login');
 }
