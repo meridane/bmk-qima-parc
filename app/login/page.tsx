3f777d7a2475b120266d7869a847d6ab7aacee2a
meridane <55077431+meridane@users.noreply.github.com>
2025-06-12 10:05:04 +0900

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,6 @@
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { supabase } from '@/lib/supabase';
-import Sidebar from '@/components/Sidebar';
-import Navbar from '@/components/Navbar';
-
 
 export default function LoginPage() {
   const router = useRouter();
@@ -30,25 +27,26 @@ export default function LoginPage() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-orange-500 to-black">
-      <div className="bg-white p-10 rounded-xl shadow-lg max-w-sm w-full">
+      <div className="bg-white p-10 rounded-2xl shadow-2xl max-w-sm w-full">
         <img src="/assets/logo.png" alt="BMK Logo" className="w-32 mx-auto mb-6" />
-        <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
+
+        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
           Connexion à BMK Qima Parc
         </h1>
 
         <div className="flex gap-4 justify-center mb-6">
           <button
             onClick={() => setRole('client')}
-            className={`px-4 py-2 rounded-xl text-white font-semibold ${
-              role === 'client' ? 'bg-orange-600' : 'bg-gray-500'
+            className={`flex-1 px-4 py-2 rounded-xl text-white font-semibold transition ${
+              role === 'client' ? 'bg-orange-600' : 'bg-gray-500 hover:bg-orange-500'
             }`}
           >
             Client
           </button>
           <button
             onClick={() => setRole('admin')}
-            className={`px-4 py-2 rounded-xl text-white font-semibold ${
-              role === 'admin' ? 'bg-black' : 'bg-gray-500'
+            className={`flex-1 px-4 py-2 rounded-xl text-white font-semibold transition ${
+              role === 'admin' ? 'bg-black' : 'bg-gray-500 hover:bg-black'
             }`}
           >
             Superviseur
@@ -57,7 +55,7 @@ export default function LoginPage() {
 
         <button
           onClick={handleLogin}
-          className="w-full bg-black hover:bg-orange-600 text-white py-2 px-4 rounded-xl"
+          className="w-full bg-black hover:bg-orange-600 text-white py-3 px-4 rounded-xl font-semibold transition"
         >
           Connexion avec Google
         </button>
