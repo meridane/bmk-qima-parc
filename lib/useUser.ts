ae49e49ec04ee2a9dd8a09cbd9df5635e521ec65
meridane <55077431+meridane@users.noreply.github.com>
2025-06-13 09:58:07 +0900

    Create useUser.ts

---
+++ lib/useUser.ts
@@ -0,0 +1,45 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { supabase } from './supabase';
+import { Session, User } from '@supabase/supabase-js';
+
+export default function useUser() {
+  const [user, setUser] = useState<User | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+
+      if (error) {
+        console.error('Erreur récupération session:', error.message);
+        setUser(null);
+        setSession(null);
+      } else {
+        setSession(session);
+        setUser(session?.user ?? null);
+      }
+      setLoading(false);
+    };
+
+    getSession();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+        setUser(session?.user ?? null);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return { user, session, loading };
+}
