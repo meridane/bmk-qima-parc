55a6c0c776e456d55b4f96438fb8577f600f6515
meridane <55077431+meridane@users.noreply.github.com>
2025-06-13 10:21:21 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-'use client';
-
-import Providers from './providers';
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
-import { useEffect } from 'react';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,17 +10,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // Initialise Supabase pour que les cookies soient bien pris en compte
-    createBrowserSupabaseClient();
-  }, []);
-
   return (
     <html lang="fr">
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
