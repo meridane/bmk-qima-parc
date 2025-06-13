9a11dc288c403c651ce5a3dd33ca83287687794c
meridane <55077431+meridane@users.noreply.github.com>
2025-06-13 09:10:30 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Providers from "./providers";
-import "./globals.css";
+import "./styles/globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
