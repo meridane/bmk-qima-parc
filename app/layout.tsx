1ca77b62bdbc66b6bf47ff20894abc9e3e85a8cc
meridane <55077431+meridane@users.noreply.github.com>
2025-06-13 10:12:29 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
-import Providers from "./providers";
-import "../styles/globals.css";
-import { Inter } from "next/font/google";
+'use client';
 
-const inter = Inter({ subsets: ["latin"] });
+import Providers from './providers';
+import '../styles/globals.css';
+import { Inter } from 'next/font/google';
+import { useEffect } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "BMK Qima Parc",
-  description: "Plateforme de gestion de conteneurs",
+  title: 'BMK Qima Parc',
+  description: 'Plateforme de gestion de conteneurs',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    // Initialise Supabase pour que les cookies soient bien pris en compte
+    createBrowserSupabaseClient();
+  }, []);
+
   return (
     <html lang="fr">
       <body className={inter.className}>
