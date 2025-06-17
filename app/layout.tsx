b3acca5c0f708da67f31a936446938d1c08dced4
meridane <55077431+meridane@users.noreply.github.com>
2025-06-17 18:15:33 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,26 @@
 // Third-party Imports
 import 'react-perfect-scrollbar/dist/css/styles.css'
 
-// Type Imports
-type ChildrenType = {
-  children: React.ReactNode
-}
-
 // Style Imports
 import '../styles/globals.css'
 import '../styles/generated-icons.css'
 
-// Materio Theme Wrapper
-import ThemeComponent from './theme/ThemeComponent'
-
 export const metadata = {
   title: 'BMK Qima Parc',
-  description: 'Gestion des conteneurs pour l’export de voitures.'
+  description: 'Plateforme de gestion pour le chargement des conteneurs BMK.'
+}
+
+// ✅ Type direct ici
+type ChildrenType = {
+  children: React.ReactNode
 }
 
 const RootLayout = ({ children }: ChildrenType) => {
+  const direction = 'ltr'
+
   return (
-    <html lang="fr">
-      <body>
-        <ThemeComponent>{children}</ThemeComponent>
-      </body>
+    <html id='__next' dir={direction}>
+      <body className='flex is-full min-bs-full flex-auto flex-col'>{children}</body>
     </html>
   )
 }
