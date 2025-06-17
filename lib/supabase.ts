750cab0d0a8f57aff59f9661e6b5c9032a7e7061
meridane <55077431+meridane@users.noreply.github.com>
2025-06-18 00:09:46 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-import type { Database } from './types/supabase'  // le fichier généré
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from '@/types/supabase' // ← très important
 
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
-
-export default supabase
+export const supabase = createClientComponentClient<Database>()
