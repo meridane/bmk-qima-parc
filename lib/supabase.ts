5e1b43122afdec28c73df34a0333f8eabed5081f
meridane <55077431+meridane@users.noreply.github.com>
2025-06-17 19:05:18 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr'
+import type { Database } from '@/types/supabase'  // adapte ce chemin si besoin
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createBrowserClient<Database>()
