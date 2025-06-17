fca0ea99011c806d45972ac9196e5f59558b48a3
meridane <55077431+meridane@users.noreply.github.com>
2025-06-17 23:37:21 +0900

    Update supabase.ts

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,1 +1,41 @@
 export type Database = any;
+
+clients: {
+  Row: {
+    id: string
+    email: string | null
+    nom_complet: string | null
+    tel: string | null
+    whatsapp: string | null
+    same_number: boolean | null
+    langue_native: string | null
+    volume_conteneurs: number | null
+    password_hash: string | null
+    status: string | null
+    created_at: string | null
+  }
+  Insert: {
+    email?: string | null
+    nom_complet?: string | null
+    tel?: string | null
+    whatsapp?: string | null
+    same_number?: boolean | null
+    langue_native?: string | null
+    volume_conteneurs?: number | null
+    password_hash?: string | null
+    status?: string | null
+    created_at?: string | null
+  }
+  Update: {
+    email?: string | null
+    nom_complet?: string | null
+    tel?: string | null
+    whatsapp?: string | null
+    same_number?: boolean | null
+    langue_native?: string | null
+    volume_conteneurs?: number | null
+    password_hash?: string | null
+    status?: string | null
+    created_at?: string | null
+  }
+}
