e624d6aedd42b7ac6d58f336f9351bffff00a9b0
meridane <madda.yasser@gmail.com>
2025-06-18 11:37:09 +0900

    fix: cast theme object with 'as unknown as Theme'

--- components/@core/theme/index.ts
+++ components/@core/theme/index.ts
@@ -1,28 +1,9 @@
-// Next Imports
-import { Inter } from 'next/font/google'
-
-// MUI Imports
-import type { Theme } from '@mui/material/styles'
-
-// Type Imports
-import type { SystemMode } from '@core/types'
-
-// Theme Options Imports
-import overrides from './overrides'
-import colorSchemes from './colorSchemes'
-import spacing from './spacing'
-import shadows from './shadows'
-import customShadows from './customShadows'
-import typography from './typography'
-
-const inter = Inter({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700', '800', '900'] })
-
-const theme = (mode: SystemMode, direction: Theme['direction']): Theme => {
+const theme = (mode: SystemMode, direction: Theme['direction']) => {
   return {
     direction,
     components: overrides(),
     colorSchemes: colorSchemes(),
-    ...spacing,
+    customShadows: customShadows(mode),
     shape: {
       borderRadius: 6,
       customBorderRadius: {
@@ -30,19 +11,10 @@ const theme = (mode: SystemMode, direction: Theme['direction']): Theme => {
         sm: 4,
         md: 6,
         lg: 8,
-        xl: 10
+        xl: 12
       }
     },
     shadows: shadows(mode),
-    typography: typography(inter.style.fontFamily),
-    customShadows: customShadows(mode),
-    mainColorChannels: {
-      light: '46 38 61',
-      dark: '231 227 252',
-      lightShadow: '46 38 61',
-      darkShadow: '19 17 32'
-    }
-  } as Theme
+    typography: typography()
+  } as unknown as Theme
 }
-
-export default theme
